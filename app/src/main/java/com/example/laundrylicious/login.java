7b0b7d5ae8daa2ad557eae1d49766f6927d5d4a1
lawakobadu <73542361+lawakobadu@users.noreply.github.com>
2021-10-05 03:00:10 +0700

    p

---
+++ app/src/main/java/com/example/laundrylicious/login.java
@@ -0,0 +1,25 @@
+package com.example.laundrylicious;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.login);
+    }
+
+    public void pindah(View view) {
+        Intent intent = new Intent(login.this, register.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/laundrylicious/register.java
@@ -0,0 +1,25 @@
+package com.example.laundrylicious;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.register);
+    }
+
+    public void kembali(View view) {
+        startActivity(new Intent(this, login.class));
+        finish();
+    }
+}
