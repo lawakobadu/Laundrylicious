95c7a601317489bc54b4cc3e3f42e6d1f0fd6e73
lawakobadu <73542361+lawakobadu@users.noreply.github.com>
2021-11-17 14:21:52 +0700

    tes

--- app/src/main/java/com/example/laundrylicious/profile.java
+++ app/src/main/java/com/example/laundrylicious/profile.java
@@ -1,27 +1,13 @@
 package com.example.laundrylicious;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public class profile extends AppCompatActivity {
-
-    ImageView imageView;
-    int photo = 1;
-    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +16,6 @@ public class profile extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.profile);
-
-        ImageView imageView35 = findViewById(R.id.imageView35);
-        imageView = findViewById(R.id.circularImageView);
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent,photo);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == photo && resultCode == RESULT_OK && data != null && data.getData() != null ){
-            uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                imageView.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public void kembali(View view) {
--- app/src/main/java/com/example/laundrylicious/verifikasi.java
+++ app/src/main/java/com/example/laundrylicious/verifikasi.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class verifikasi extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
