f9e38ac9dc5f9cd5e9e1dcf28c6f73657b489be7
Linaqruf <furqanil.taqwa@gmail.com>
2022-01-10 23:22:06 +0700

    Membuat autentikasi menggunakan SQLite

--- app/src/main/java/com/example/laundrylicious/lupa_password.java
+++ app/src/main/java/com/example/laundrylicious/lupa_password.java
@@ -1,30 +1,30 @@
-package com.example.laundrylicious;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class lupa_password extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-        setContentView(R.layout.lupa_password);
-    }
-
-    public void kembali(View view) {
-        startActivity(new Intent(this, login.class));
-        finish();
-    }
-
-    public void verifikasi(View view) {
-        startActivity(new Intent(this, verifikasi.class));
-        finish();
-    }
-}
+//package com.example.laundrylicious;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.view.Window;
+//import android.view.WindowManager;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class lupa_password extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide();
+//        setContentView(R.layout.lupa_password);
+//    }
+//
+//    public void kembali(View view) {
+//        startActivity(new Intent(this, login.class));
+//        finish();
+//    }
+//
+//    public void verifikasi(View view) {
+//        startActivity(new Intent(this, verifikasi.class));
+//        finish();
+//    }
+//}
